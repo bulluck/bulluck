39f96fa93bf91a8682926f4c0081d18e820d8b22
takerusekine <t.0327.s@gmail.com>
2015-10-24 18:29:12 +0900

    sslに対応するための更新。一行目が今回更新されたところ。LINEのお問い合わせ等は、取り込み済み。

--- support.php
+++ support.php
@@ -1,14 +1,66 @@
+<?php require '../../../wp-blog-header.php';?>
 <?php get_header(); ?>
-<section id="otoiawase">
 
-<div class="contentsbody">
-<span style="font-size:2rem;text-align:center;"><?php the_title(); ?></span>
-<?php echo do_shortcode('[contact-form-7 id="69" title="お問い合わせフォーム"]'); ?>
-<div class="otoiawasemail">
 
-    <a href="mailto:info@bulluck.co.jp">
-        <img alt="" src="<?php bloginfo('template_url');?>/images/mailbannar.png">
-    </a>
-</div>
-</div>
+<section id="contact">
+    <div class="wrapnormal" style="padding-bottom:0;">
+        <div class="formcontentsbody">
+        <h3 class="boxtitle">Contact</h3>
+        <h4>お問い合わせはコチラへ</h4>
+    </div>
+    <?php if (!wp_is_mobile()): ?>
+        <div class="lineQR">
+            <div class="icon">
+                <img alt="LINEアイコン" src="<?php bloginfo('template_url');?>/images/LINE_icon.png" width="100">
+            </div>
+            <div class="linetable" id="line">
+                <div class="linetblcell" style="vertical-align:top;">
+                    <p style="margin:0 auto;font-size:2rem;">
+                        a. LINE QRコード
+                    </p>
+                    <img alt="Line QRcode" src="<?php bloginfo('template_url');?>/images/line_qrcode.png">
+                    <p style="margin:0 auto;">LINEアプリの<br>「その他」>「友だち追加」>「QRコード」</p>
+                </div>
+                <div>
+                        <p style="font-size:2rem;margin-top:10%;">
+                            ：<br>
+                            ：<br>
+                            OR<br>
+                            ：<br>
+                            ：<br>
+                        </p>
+                </div>
+                <div class="linetblcell" style="vertical-align:top;">
+                    <p style="margin:0 auto;font-size:2rem;">
+                        b. LINE ID検索
+                    </p>
+                    <p style="margin:10% auto;font-size:2rem;font-weight:bold;color:#1B1464;">
+                            @yrd9480x
+                    </p>
+                    <p>
+                        LINEアプリの<br>「その他」>「友だち追加」>「ID検索」
+                    </p>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+        <div class="mailform" id="mail">
+            <div class="icon">
+                <img alt="メールアイコン" src="<?php bloginfo('template_url');?>/images/mail.png">
+            </div>
+
+                    <p>
+                        問い合わせフォームはこちら
+                    </p>
+                    <?php echo do_shortcode('[contact-form-7 id="69" title="お問い合わせフォーム"]'); ?>
+                    <p>
+                        メールアドレスはこちら
+                    </p>
+                    <a style="font-size:2rem;text-decoration:none;" href="mailto:info@bulluck.co.jp">
+                            info@bulluck.co.jp
+                    </a>
+
+        </div>
+        </div>
+    </div>
 </section>
