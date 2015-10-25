903081ba6caa0b33049d3afdeed0235e93108557
miyagawakazuki <miyagawakazuki@miyagawakazuki-no-MacBook-Air.local>
2015-10-26 07:14:07 +0900

    h1タグ調整

--- support.php
+++ support.php
@@ -5,7 +5,14 @@
 
 
 <div class="contentsbody">
+<h1 class="toi">
+  お問い合わせ
+</h1>
+
+
 <span style="font-size:2rem;text-align:center;"><?php the_title(); ?></span>
+
+
 <?php echo do_shortcode('[contact-form-7 id="69" title="お問い合わせフォーム"]'); ?>
 <div class="otoiawasemail">
 
