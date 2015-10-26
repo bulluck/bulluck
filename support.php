0a07b3f63c4879168fa1a8a662b2aae1c06f7b24
miyagawakazuki <miyagawakazuki@miyagawakazuki-no-MacBook-Air.local>
2015-10-26 10:50:45 +0900

    no message

--- support.php
+++ support.php
@@ -93,15 +93,12 @@
         </div>
     </div>
 </section>
-
-
-
-<?php echo do_shortcode('[contact-form-7 id="69" title="お問い合わせフォーム"]'); ?>
 <div class="otoiawasemail">
 
     <a href="mailto:info@bulluck.co.jp">
         <img alt="" src="<?php bloginfo('template_url');?>/images/mailbannar.png">
     </a>
 </div>
+
 </div>
 </section>
